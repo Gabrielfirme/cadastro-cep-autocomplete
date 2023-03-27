9b2f80f5c075090b10e3ed3596cb956d2e43df4b
GABRIEL FIRME <94994830+Gabrielfirme@users.noreply.github.com>
2023-03-27 10:27:02 -0300

    :lips: :construction:  inicio da construção do js
    - selecionando elementos de forma separada;
    - único elemento a ser selecionado com queryselectall somente [data-input].

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log("linkado");
+const addresForm = document.querySelector("#addres-form");
+const cepInput = document.querySelector("#cep");
+const addresInput = document.querySelector("#addres");
+const cityInput = document.querySelector("#city");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector ("#region");
+const formInputs = document.querySelectorAll ("[data-input]");
+
+const closeButton = document.querySelector("#close-message");
