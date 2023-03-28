6de75c68b13029e2c9e2078b67ee4fbdf31a75a6
GABRIEL FIRME <94994830+Gabrielfirme@users.noreply.github.com>
2023-03-28 16:36:00 -0300

    :recycle: :refactor: alterando classes
    * alterando classes;
    * otimizando estilos.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,15 @@ const cityInput = document.querySelector("#city");
 const neighborhoodInput = document.querySelector("#neighborhood");
 const regionInput = document.querySelector ("#region");
 const formInputs = document.querySelectorAll ("[data-input]");
-
 const closeButton = document.querySelector("#close-message");
 
-//validação do  CEP input//
+
 
 cepInput.addEventListener("keypress", (e) => {
     const onlyNumbers = /[0-9]/;
         const key = String.fromCharCode(e.keyCode);
         
-   //permitir somente números//
+   
 
 if (!onlyNumbers.test(key)) {
     e.preventDefault();
@@ -22,19 +21,19 @@ if (!onlyNumbers.test(key)) {
 }
 });
 
-// Get address event
+
 
 cepInput.addEventListener("keyup", (e) => {
 
         const inputValue = e.target.value
 
-        //checagem da quantidade necessária de dígitos//
+        
         if(inputValue.length === 8){
             getAddress(inputValue);
         }
 });
 
-//obter endereço do clinetre via api//
+
 
 const getAddress = async (cep) => {
    toggleLoader();
@@ -51,7 +50,7 @@ const getAddress = async (cep) => {
    console.log(formInputs);
    console.log(data.erro);
 
-//resetar o formulário em caso de erro
+
 if(data.erro === "true"){
     if (!addressInput.hasAttribute("disabled")){
         toggleDisabled();
@@ -59,10 +58,10 @@ if(data.erro === "true"){
 
     addressForm.reset();
         toggleLoader();
-        toggleMessage("CEP inválido por favor tente novamente");
+        toggleMensagem("CEP inválido por favor tente novamente");
         return;
  }
- // Ativa o atributo desativado se o formulário estiver vazio
+ 
 
     if(addressInput.value === ""){
     toggleDisabled();
@@ -76,7 +75,7 @@ if(data.erro === "true"){
     toggleLoader();
 };
 
-// Adicionar ou remover atributo desabilitado
+
 
 const toggleDisabled = () => {
     if(regionInput.hasAttribute("disabled")) {
@@ -92,7 +91,7 @@ const toggleDisabled = () => {
 };
 
 
-//mostrar o loader
+
 const toggleLoader = () => {
 
     const fadeElement = document.querySelector("#fade");
@@ -104,25 +103,25 @@ const toggleLoader = () => {
 
 };
 
-//Mostrar ou não as mensagens 
 
-const toggleMessage = (msg) => {
+
+const toggleMensagem = (msg) => {
     const fadeElement =  document.querySelector("#fade");
-    const messageElement =  document.querySelector("#message")
+    const mensagemElement =  document.querySelector("#mensagem")
 
-    const messageTextElement = document.querySelector("#message p");
+    const mensagemTextElement = document.querySelector("#mensagem p");
 
-    messageTextElement.innertext = msg;
+    mensagemTextElement.innertext = msg;
 
     fadeElement.classList.toggle("hide");
-    messageElement.classList.toggle("hide");
+    mensagemElement.classList.toggle("hide");
 };
 
-// Fechando meensagem modal 
 
-closeButton.addEventListener("click", () => toggleMessage());
 
-//Salvar Endereço
+closeButton.addEventListener("click", () => toggleMensagem());
+
+
 
 addressForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -132,7 +131,7 @@ addressForm.addEventListener("submit", (e) => {
     setTimeout(() => {
         toggleLoader();
 
-        toggleMessage("Endereço Salvo com sucesso!");
+        toggleMensagem("Endereço Salvo com sucesso!");
 
         addressForm.reset();
 
